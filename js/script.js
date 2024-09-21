2556f1dfafd588480401f3d5ed6721cade9aa911
Carrie Furet <carrie.furet@gmail.com>
2024-09-20 18:14:14 -0700

    completed steps 1 - 22

---
+++ js/script.js
@@ -0,0 +1,172 @@
+// STEP 1
+// someMonth
+// theMonth()
+// currentMonth
+// summerMonth
+// myLibraryFunction
+
+
+// STEP 2
+// numeric literal expression = 2
+// string literal expression = 'hello'
+// boolean literal expression = true
+// null literal expression = null
+
+
+// STEP 3
+// complex expression = 5 + 9
+//    = 2x - y
+
+
+// STEP 4
+// strFirstName
+// strLastName
+// strAddress
+// strCity
+// strState 
+// nZipCode
+// nYourAge
+// strReferralSource
+// bMayWeContactYou
+
+
+// STEP 5
+// let strFirstName
+// let nZipCode
+// let bMayWeContactYou
+// strFirstName = 'Betty Sue'
+// nZipCode = 92145
+// bMayWeContactYou = false
+
+// let strFirstName = 'Betty Sue'
+// let nZipCode = 92145
+// let bMayWeContactYou = false
+
+// let strFirstName = 'Betty Sue', nZipCode = 92145, bMayWeContactYou = false
+
+
+// STEP 6
+// let test 
+// test = 'funny'
+// document.write(test + 7)
+// console.log(test + 7)
+// I couldn't get my console.log to work for some reason so I also tested with document.write
+
+
+// STEP 7
+// let first = true + 'honey'
+// let second = 7 + true
+
+// document.write(first)
+// document.write(second)
+
+
+// STEP 8
+// No, here is the correct string
+// let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+// document.write(someString)
+// console.log(someString)
+
+
+// STEP 9
+// let x = null
+// let y
+
+// document.write(x)
+// document.write(y)
+// console.log(x)
+// console.log(y)
+
+
+// STEP 10
+// document.write(typeof 'hello')
+// document.write(typeof 67)
+// document.write(typeof false)
+// document.write(typeof new Object())
+// document.write(typeof x)
+
+// console.log(typeof 'hello')
+// console.log(typeof 67)
+// console.log(typeof false)
+// console.log(typeof new Object())
+// console.log(typeof x)
+
+
+// STEP 11
+// alert('Hello ' + 'Carrie Furet' + ', welcome to the JavaScript class!')
+
+
+// STEP 12
+// let myName = 'Carrie Furet'
+// alert('Hello ' + myName + ', welcome to the JavaScript class!')
+
+
+// STEP 13
+// let myName = 'Carrie Furet'
+// let course = 'JavaScript'
+// alert('Hello ' + myName + ', welcome to the ' + course + ' class!')
+
+
+// STEP 14
+// let myName = 'Carrie Furet'
+// let course = 'JavaScript'
+// alert('Hello ' + myName + '.\nWelcome to the ' + course + ' class!')
+
+
+// STEP 15
+// let myName = prompt('What is your name?')
+// let course = 'JavaScript'
+// alert('Hello ' + myName + '.\nWelcome to the ' + course + ' class!')
+
+
+// STEP 16
+// let myName = prompt('What is your name?')
+// let course = prompt('What class are you taking?')
+// alert('Hello ' + myName + '.\nWelcome to the ' + course + ' class!')
+
+
+// STEP 17
+// let x = 10
+// let y = 20
+// document.write(x + y)
+// console.log(x + y)
+
+
+// STEP 18
+// let x = 20
+// x += 20
+// document.write(x)
+// console.log(x)
+
+
+// STEP 19
+// let x = 20
+// x *= 5
+// document.write(x)
+// console.log(x)
+
+
+// STEP 20
+// let x = 20 % 3
+// x /= 1
+// document.write(x)
+// console.log(x)
+
+
+// STEP 21
+// let x = 10, y = 5
+// if (x > y) {
+//     document.write('Yes, ' + x + ' is greater than ' + y)
+//     console.log('Yes, ' + x + ' is greater than ' + y)
+// }
+
+
+// STEP 22
+// let x = 10, y = 5
+// if (x == y) {
+//     document.write('Yes, ' + x + ' equals ' + y)
+//     console.log('Yes, ' + x + ' equals ' + y)
+// } else {
+//     document.write('No, ' + x + ' does not equal ' + y)
+//     console.log('No, ' + x + ' does not equal ' + y) 
+// }
